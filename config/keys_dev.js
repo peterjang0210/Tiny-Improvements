013a066116b8ba0b4d43269bafc4182be2906fec
Peter Jang <peter.jang@outlook.com>
2019-03-03 22:01:10 -0500

    configured connection to mongodb for deployment

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+
+    
+module.exports = {
+    mongoURI: "mongodb+srv://peterjang:funfunfun@cluster0-akaol.mongodb.net/test?retryWrites=true",
+    secretOrKey: 'SECRET'
+  };
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const MONGODB_URI = require('./config/keys');
+mongoose.Promise = global.Promise;
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -9,8 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+mongoose.connect(
+  MONGODB_URI || "mongodb://localhost/tinyImprovements",
+  {
+    useMongoClient: true
+  }
+)
 
-mongoose.connect('mongodb://localhost/tinyImprovements', { useNewUrlParser: true });
+
+//mongoose.connect('mongodb://localhost/tinyImprovements', { useNewUrlParser: true });
 
 require('./routes/html-routes')(app);
 require('./routes/api-routes')(app);
