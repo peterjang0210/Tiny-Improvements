bfeaa34055e1a23560d507e447f2c3c2b589b448
Peter Jang <peter.jang@outlook.com>
2019-03-04 13:25:20 -0500

    deleted not working seed file

--- mongo-seeds.js
+++
@@ -1,8 +0,0 @@
-use tinyImprovements
-
-db.User.insert({name: "Peter"})
-db.User.insert({name: "Bob"})
-db.User.insert({name: "Joe"})
-db.User.insert({name: "Jim"})
-db.User.insert({name: "Daniel"})
-
